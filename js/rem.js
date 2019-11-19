69194f11ec397642a430d1c8e269fbdd67a6459e
“e <497682226@qq.com>
2019-11-19 18:40:21 +0800

    about

--- js/rem.js
+++ js/rem.js
@@ -1,5 +1,4 @@
 $(function () {
-    debugger
     resizeLoad();
     $(window).resize(function(){
         resizeLoad();
@@ -9,8 +8,7 @@ $(function () {
         var width = $(window).width();
 
         //var fontSize =30* (width/720).toFixed(2);
-        var fontSize =(width/100).toFixed(2);
-        debugger
+        var fontSize =(width/20).toFixed(2);
 
         //if(width<720)
         //{
